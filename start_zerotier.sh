70ed52919210c297d2b8782736575ca91352b1d4
JakeInit <jmorgan@cuesinc.com>
2023-03-13 14:37:52 -0400

    Create Environment Examples
    Add windows support and setup

---
+++ start_zerotier.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SUB='200'
+
+service zerotier-one start
+sleep 1
+STATUS="$(zerotier-cli status)"
+while echo "$SUB" | grep -q "$STATUS"; do
+    sleep 1
+    STATUS="$(zerotier-cli status)"
+done
+
+# Need to create a network for zerotier to use this file
+zerotier-cli join <networkID>
+export CONAN_SYSREQUIRES_MODE=enabled
